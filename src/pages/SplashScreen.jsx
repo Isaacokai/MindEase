2778c91b0c40bcb0a940f26d2fa7ba9266285500
Isaacokai <89702305+Isaacokai@users.noreply.github.com>
2025-07-30 17:48:20 +0000

    Refactor image source in SplashScreen component to use imported LogoImage

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -1,11 +1,13 @@
+
 import { Link } from "react-router-dom";
 import Button from "../components/Button";
 import "../splash.css";
+import LogoImage from "../assets/Logo (1).png";
 
 const SplashScreen = () => {
   return (
     <div className="splash-screen">
-      <img src="src/assets/Logo (1).png" alt="MindEase Logo" className="logo" />
+      <img src={LogoImage} alt="MindEase Logo" className="logo" />
       <Link to="/splash2">
         <Button>Get Started</Button>
       </Link>
